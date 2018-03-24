b8d38cdde17533a02e5f2e89daf6f8c0097dee88
impshum <impshum@impshum.co.uk>
2018-03-24 00:42:27 +0000

    Update run.py

--- run.py
+++ run.py
@@ -4,28 +4,25 @@ from random import shuffle
 script_dir = os.path.dirname(__file__)
 dir1_path = 'test1/'
 dir2_path = 'test2/'
-new_filenames_path = 'new_filenames.txt'
-new_filenames = os.path.join(script_dir, new_filenames_path)
 dir1 = os.path.join(script_dir, dir1_path)
 dir2 = os.path.join(script_dir, dir2_path)
 ext = '.PAC'
 
-with open(new_filenames) as f:
-    fnames = f.readlines()
+fnames = os.listdir(dir1)
 
 def rename(re1, re2):
     file_list = os.listdir(re1)
     files = []
+
 
     for file in file_list:
         files.append(file)
 
     shuffle(files)
     shuffle(fnames)
-
     for x, y in zip(files, fnames):
         a = re1 + x
-        b = re2 + y + ext
+        b = re2 + y
         os.rename(a, b)
 
     print(str(len(fnames)) + '/' + str(len(files)))
